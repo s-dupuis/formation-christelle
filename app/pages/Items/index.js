4be4200eba4d85350bb5398596dcf259ced579ee
Christelle <christelle.hognon@fasst.io>
2023-02-28 12:17:36 +0100

    FC-10 corrections PR

--- app/pages/Items/index.js
+++ app/pages/Items/index.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import useItems from '../../hooks/useItems';
 import { useForm } from 'react-hook-form';
 import CreateItemMutation from '../../_graphql/mutations/item/CreateItemMutation';
-import { getValue } from 'relay-runtime/lib/store/RelayModernRecord';
 
 const R = require('ramda');
 const Items = () => {
   const data = useItems().items;
+  const isNotEmpty = R.length(data.items) > 0;
  const [isModalOpen, setIsModalOpen] = useState(false);
 
  const { register, handleSubmit, formState: { errors } } = useForm();
@@ -18,38 +18,40 @@ const Items = () => {
           <table>
             <thead>
               <tr className="hover:border-collapse">
-                <th scope="col" className="bg-blue-100 border text-left px-8 py-4">Item ID</th>
-                <th scope="col" className="bg-blue-100 border text-left px-8 py-4">Item Name</th>
-                <th scope="col" className="bg-blue-100 border text-left px-8 py-4">Item Category</th>
-                <th scope="col" className="bg-blue-100 border text-left px-8 py-4">Item Group</th>
+                <th scope="col" className="f-item-table-th">Item ID</th>
+                <th scope="col" className="f-item-table-th">Item Name</th>
+                <th scope="col" className="f-item-table-th">Item Category</th>
+                <th scope="col" className="f-item-table-th">Item Group</th>
               </tr>
             </thead>
             <tbody>
-              {
-                R.map((item) =>
+              {!isNotEmpty ?
+                <span>Aucun item à afficher</span>
+                : <>
+                {R.map((item) =>
                   <tr key={item.id}>
-                    <td className="border text-center px-8 py-4">{item?.id}</td>
-                    <td className="border text-center px-8 py-4">{item?.name}</td>
-                    <td className="border text-center px-8 py-4">{item?.category}</td>
-                    <td className="border text-center px-8 py-4">{item?.group}</td>
+                    <td className="f-item-table-td">{item?.id}</td>
+                    <td className="f-item-table-td">{item?.name}</td>
+                    <td className="f-item-table-td">{item?.category}</td>
+                    <td className="f-item-table-td">{item?.group}</td>
                   </tr>
                   , data.items)
+                }
+                </>
               }
             </tbody>
           </table>
         </h1>
             {!isModalOpen ?
               <button
-                className="bg-purple-500 text-white rounded-md px-8 py-2 text-base font-medium hover:bg-blue-600
-    focus:outline-none focus:ring-2 focus:ring-green-300" type="button" onClick={() => setIsModalOpen(true)}>Créer</button>
+                className="f-item-modal-button" type="button" onClick={() => setIsModalOpen(true)}>Créer</button>
               : <>
                 <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                   <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                       <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
                         <h3 className="text-3xl font=semibold">Créer un item</h3>
-                        <button className="bg-transparent border-0 text-black float-right" onClick={() => setIsModalOpen(false)}
-                        >
+                        <button className="bg-transparent border-0 text-black float-right" onClick={() => setIsModalOpen(false)}>
                 <span className="text-black opacity-7 h-6 w-6 text-xl block bg-white py-0 rounded-full">
                       x
                 </span>
@@ -65,7 +67,7 @@ const Items = () => {
                             });
                         })
                         }>
-                          <input {...register('name', { required: 'This is required' })}/>
+                          <input {...register('name', { required: 'This is required' })} placeholder='Name'/>
                           <p>{errors.name?.message}</p>
                           <select {...register('category')}>
                             <option value="" disabled selected>Choisir une catégories</option>
