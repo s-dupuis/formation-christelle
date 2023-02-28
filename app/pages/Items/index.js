73c7029d2878e8251f37a75d1810464b7afeccda
Christelle <christelle.hognon@fasst.io>
2023-02-28 14:09:13 +0100

    FC-10 corrections PR

--- app/pages/Items/index.js
+++ app/pages/Items/index.js
@@ -10,7 +10,13 @@ const Items = () => {
  const [isModalOpen, setIsModalOpen] = useState(false);
 
  const { register, handleSubmit, formState: { errors } } = useForm();
-
+  const ValueCellTh = (children) => {
+    return  <th className="bg-blue-100 border text-center px-8 py-4">{children}</th>;
+  };
+  const columns = ['Item ID', 'Item Name', 'Item Category', 'Item Group'];
+ const ValueCellTd = (children) => {
+    return  <td className="border text-center px-8 py-4">{children}</td>;
+  };
   return (
     <div className="flex justify-center shadow-lg bg-white">
       <div>
@@ -18,10 +24,9 @@ const Items = () => {
           <table>
             <thead>
               <tr className="hover:border-collapse">
-                <th scope="col" className="f-item-table-th">Item ID</th>
-                <th scope="col" className="f-item-table-th">Item Name</th>
-                <th scope="col" className="f-item-table-th">Item Category</th>
-                <th scope="col" className="f-item-table-th">Item Group</th>
+                {
+                  R.map(ValueCellTh, columns)
+                }
               </tr>
             </thead>
             <tbody>
@@ -29,12 +34,11 @@ const Items = () => {
                 <span>Aucun item à afficher</span>
                 : <>
                 {R.map((item) =>
-                  <tr key={item.id}>
-                    <td className="f-item-table-td">{item?.id}</td>
-                    <td className="f-item-table-td">{item?.name}</td>
-                    <td className="f-item-table-td">{item?.category}</td>
-                    <td className="f-item-table-td">{item?.group}</td>
-                  </tr>
+                    <tr key={item.id}>
+                      {
+                        R.map(ValueCellTd, R.valuesIn(item))
+                      }
+                    </tr>
                   , data.items)
                 }
                 </>
@@ -44,12 +48,13 @@ const Items = () => {
         </h1>
             {!isModalOpen ?
               <button
-                className="f-item-modal-button" type="button" onClick={() => setIsModalOpen(true)}>Créer</button>
+                className="bg-purple-500 text-white rounded-md px-8 py-2 text-base font-medium hover:bg-blue-600
+    focus:outline-none focus:ring-2 focus:ring-green-300" type="button" onClick={() => setIsModalOpen(true)}>Créer</button>
               : <>
                 <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                   <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                      <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                      <div className="flex items-start justify-between p-5 border border-solid border-gray-300 rounded">
                         <h3 className="text-3xl font=semibold">Créer un item</h3>
                         <button className="bg-transparent border-0 text-black float-right" onClick={() => setIsModalOpen(false)}>
                 <span className="text-black opacity-7 h-6 w-6 text-xl block bg-white py-0 rounded-full">
@@ -57,7 +62,7 @@ const Items = () => {
                 </span>
                         </button>
                       </div>
-                      <div className="relative p-6 flex-auto">
+                      <div className="columns-1">
                         <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full" onSubmit={
                           handleSubmit((data) => {
                             CreateItemMutation(data, (ok, err, response) => {
